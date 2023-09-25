a5bb4b96cea85b2479195e4873c5347f8ccea7fa
Xavier2p <xavier.de-place@epita.fr>
2023-09-25 19:24:27 +0200

    change a little bit the arch

---
+++ src/helpers/cli.rs
@@ -0,0 +1,99 @@
+//! This module is used to parse the arguments passed to the program.
+use clap::{Parser, Subcommand};
+use std::path::Path;
+
+/// This struct is used to parse the arguments passed to the program.
+#[derive(Debug, Parser, Clone)]
+#[command(author, version, about)]
+#[command(subcommand_required = true)]
+pub struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+
+    /// Print the comment during the execution of the program
+    #[arg(short, long)]
+    verbose: bool,
+}
+
+/// This struct is used to parse the arguments passed to the program.
+#[derive(Debug, Subcommand, Clone)]
+pub enum Commands {
+    /// Run the Fortran file passed as argument
+    #[command(arg_required_else_help = true)]
+    Run {
+        /// Path to the file to interpret
+        #[arg(value_parser = check_path)]
+        file: String,
+
+        /// Print the comment during the execution of the program
+        #[arg(short, long)]
+        verbose: bool,
+    },
+
+    /// Check the syntax of the file passed as argument
+    #[command(arg_required_else_help = true)]
+    Check {
+        /// Path to the file to interpret
+        #[arg(value_parser = check_path)]
+        file: String,
+
+        /// Threat `Warning` as `Error`
+        #[arg(long)]
+        werror: bool,
+
+        /// Print the comment during the execution of the program
+        #[arg(short, long)]
+        verbose: bool,
+    },
+}
+
+impl Cli {
+    /// This function returns the path to the file to interpret.
+    pub fn get_path(&self) -> String {
+        match &self.command {
+            Commands::Run { file, .. } => file.to_string(),
+            Commands::Check { file, .. } => file.to_string(),
+        }
+    }
+
+    /// This function returns the value of the `verbose` argument.
+    // pub fn get_verbose(&self) -> bool {
+    //     self.verbose
+    //         || match &self.command {
+    //             Commands::Run { verbose, .. } => *verbose,
+    //             Commands::Check { verbose, .. } => *verbose,
+    //         }
+    // }
+
+    /// This function returns the value of the `werror` argument.
+    pub fn get_werror(&self) -> bool {
+        match &self.command {
+            Commands::Run { .. } => false,
+            Commands::Check { werror, .. } => *werror,
+        }
+    }
+
+    /// This function returns the called command.
+    pub fn get_command(&self) -> &Commands {
+        &self.command
+    }
+
+    pub fn debug(&self) {
+        println!("Cli {{");
+        println!("    command: {:?}", self.get_command());
+        println!("    path: {}", self.get_path());
+        println!("    werror: {}", self.get_werror());
+        println!("    verbose: {}", self.verbose);
+        println!("}}");
+        println!("---- ---- ---- ---- ----");
+    }
+}
+
+/// This function checks if the file exists.
+fn check_path(input: &str) -> Result<String, String> {
+    if !Path::new(input).exists() {
+        Err(format!("No file exists with this name: `{}`", input))
+    } else {
+        Ok(input.to_string())
+    }
+}
---
+++ src/helpers/file.rs
@@ -0,0 +1,62 @@
+//! # File Traitement
+//!
+//! This module contains the `File` struct and its methods.
+use crate::helpers::cli::Cli;
+use std::fs;
+
+/// This struct contains the file's path, name, content, version and arguments.
+#[derive(Clone)]
+pub struct File {
+    path: String,
+    name: String,
+    content: String,
+    version: String,
+}
+
+/// This struct contains the file's path, name, content, version and arguments.
+impl File {
+    /// This function returns the file's name.
+    pub fn get_name(&self) -> &String {
+        &self.name
+    }
+
+    /// This function returns the file's content.
+    pub fn get_content(&self) -> &String {
+        &self.content
+    }
+
+    pub fn debug(&self) {
+        println!("File {{");
+        println!("    name: {}", self.name);
+        println!("    path: {}", self.path);
+        println!("    version: {}", self.version);
+        println!("    lines: [");
+        for line in self.content.lines() {
+            println!("        {:?}", line);
+        }
+        println!("    ]");
+        println!("}}");
+        println!("---- ---- ---- ---- ----");
+    }
+}
+
+/// This function returns the file's arguments.
+pub fn new(args: Cli) -> File {
+    let path: String = args.get_path();
+    let file_name_with_extension: &str = path.split('/').last().unwrap();
+
+    return File {
+        path: path.clone(),
+        name: file_name_with_extension
+            .split('.')
+            .next()
+            .unwrap()
+            .to_string(),
+        content: fs::read_to_string(path.clone()).unwrap(),
+        version: file_name_with_extension
+            .split('.')
+            .last()
+            .unwrap()
+            .to_string(),
+    };
+}
