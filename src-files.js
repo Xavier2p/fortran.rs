d40e7df8aeeb5d937f8572e3b437f877faa2ef3f
Xavier2p <Xavier2p@users.noreply.github.com>
2023-10-19 11:14:49 +0000

    deploy: 4d3c9b19f6945a507d0c4bd260012ef24d5bad4f

--- src-files.js
+++ src-files.js
@@ -1,4 +1,4 @@
-var sourcesIndex = JSON.parse('{\
+var srcIndex = JSON.parse('{\
 "fortran_rs":["",[["helpers",[],["cli.rs","errors.rs","file.rs","mod.rs"]],["utils",[],["mod.rs","print.rs"]]],["ast.rs","lexer.rs","main.rs","parser.rs","program.rs","tokenizer.rs","tokens.rs","validation.rs","variables.rs","verbose.rs"]]\
 }');
-createSourceSidebar();
+createSrcSidebar();
