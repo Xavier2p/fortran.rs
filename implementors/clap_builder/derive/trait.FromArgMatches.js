9a82b717daec6708dcb449467730df2c32745632
Xavier2p <Xavier2p@users.noreply.github.com>
2023-10-29 19:16:27 +0000

    deploy: 781060669c25bcd21c3d460001a6370fb5e7d8b6

--- implementors/clap_builder/derive/trait.FromArgMatches.js
+++ implementors/clap_builder/derive/trait.FromArgMatches.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"fortran_rs":[["impl FromArgMatches for <a class=\"enum\" href=\"fortran_rs/helpers/cli/enum.Commands.html\" title=\"enum fortran_rs::helpers::cli::Commands\">Commands</a>"],["impl FromArgMatches for <a class=\"struct\" href=\"fortran_rs/helpers/cli/struct.Cli.html\" title=\"struct fortran_rs::helpers::cli::Cli\">Cli</a>"]]
+"fortran_rs":[["impl FromArgMatches for <a class=\"struct\" href=\"fortran_rs/helpers/cli/struct.Cli.html\" title=\"struct fortran_rs::helpers::cli::Cli\">Cli</a>"],["impl FromArgMatches for <a class=\"enum\" href=\"fortran_rs/helpers/cli/enum.Commands.html\" title=\"enum fortran_rs::helpers::cli::Commands\">Commands</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
